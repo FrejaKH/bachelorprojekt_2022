376b42b6af1c9eae61284fa4f36f5bc85d0cbac1
Freja <freja.hyldgaard@gmail.com>
2022-11-11 15:22:17 +0100

    second form made

--- components/BudgetForm.vue
+++ components/BudgetForm.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="budgetform_container">
     <div>
-        <h4>Beregn din samlede udgift for denne bil</h4>
+        <h4>Beregn udgiften/budgettet for denne bil</h4>
     </div>
-    <div class="form_btn_container" :class="[isOpen ? 'isOpen' : 'notOpen']">
-        <button v-if="!isOpen" class="form_btn" :class="[isOpen ? 'isOpen_btn' : 'notOpen_btn']" @click="toggle"><strong>Beregn udgift inkl. førstegangsydelse</strong><i class="fa fa-angle-down"></i></button>
-        <button v-else class="form_btn" :class="[isOpen ? 'isOpen_btn' : 'notOpen_btn']" @click="toggle"><strong>Beregn udgift inkl. førstegangsydelse</strong><i class="fa fa-angle-up"></i></button>
+    <div class="form_btn_container" :class="[isOpen_1 ? 'isOpen' : 'notOpen']">
+        <button v-if="!isOpen" class="form_btn" @click="toggle1"><strong>Beregn udgiften/budgettet for at lease denne bil</strong><i class="fa fa-angle-down"></i></button>
+        <button v-else class="form_btn" @click="toggle1"><strong>Beregn udgiften/budgettet for at lease denne bil</strong><i class="fa fa-angle-up"></i></button>
         <form>
             <div class="form_input">
                 <label>Førstegangsydelse</label>
-                <input type="text" v-model="firstPayment">
+                <input type="text" v-model="firstPayment" readonly>
                 <label>kr.</label>
                 <label><i class="fa fa-info-circle"></i></label>
             </div>
             <div class="form_input">
-                <label>Leasing pr. måned</label>
-                <input type="text" v-model="monthlyPayment">
+                <label>Leasingpris pr. måned</label>
+                <input type="text" v-model="monthlyPayment" readonly>
                 <label>kr.</label>
                 <label><i class="fa fa-info-circle"></i></label>
             </div>
             <div class="form_input">
                 <label>Grøn ejerafgift</label>
-                <input type="text" v-model="ejerafgift">
+                <input type="text" v-model="ejerafgift" readonly>
                 <label>kr. pr. måned</label>
                 <label><i class="fa fa-info-circle"></i></label>
             </div>
@@ -32,8 +32,8 @@
                 <label><i class="fa fa-info-circle"></i></label>
             </div>
             <div class="form_input">
-                <label>Vælg længde på udgiftperiode:</label>
-                <input type="number" v-model="periode">
+                <label>Ønsket budgetperiode:</label>
+                <input type="number" v-model="periode" min="1">
                 <label>måneder</label>
                 <label><i class="fa fa-info-circle"></i></label>
             </div>
@@ -41,14 +41,42 @@
                 <label>Total:</label>
                 <input v-if="forsikring && periode" type="text" v-model="total">
                 <input v-else type="text">
-                <label>kr. for {{periode}} måneder inkl. førstegangsydelse</label>
+                <label v-if="periode > 1">kr. for {{periode}} måneder inkl. førstegangsydelse</label>
+                <label v-else>kr. for {{periode}} måned inkl. førstegangsydelse</label>
+            </div>
+        </form>
+    </div>
+    <div class="form_btn_container" :class="[isOpen_2 ? 'isOpen' : 'notOpen']">
+        <button v-if="!isOpen" class="form_btn" @click="toggle2"><strong>Beregn årligt brændstofforbrug</strong><i class="fa fa-angle-down"></i></button>
+        <button v-else class="form_btn" @click="toggle2"><strong>Beregn årligt brændstofforbrug</strong><i class="fa fa-angle-up"></i></button>
+        <form>
+            <div class="form_input">
+                <label>Antal km årligt</label>
+                <input type="text" v-model="kmYear">
+                <label>km</label>
+                <label><i class="fa fa-info-circle"></i></label>
+            </div>
+            <div class="form_input">
+                <label>Brændstofpris</label>
+                <input type="text" v-model="fuelPrice">
+                <label>kr./l</label>
+                <label><i class="fa fa-info-circle"></i></label>
+            </div>
+            <div class="form_input">
+                <label>Km/l</label>
+                <input type="text" v-model="carData.fuelEfficiency" readonly>
+                <label>km/l</label>
+                <label><i class="fa fa-info-circle"></i></label>
+            </div>
+            <div class="result">
+                <label>Total:</label>
+                <input v-if="kmYear && fuelPrice" type="text" v-model="total_fuel">
+                <input v-else type="text">
+                <label>kr. årligt</label>
             </div>
         </form>
     </div>
     <!-- <div class="form_btn_container">
-        <button>Beregn årligt brændstofforbrug<i class="fa fa-angle-down"></i></button>
-    </div>
-    <div class="form_btn_container">
         <button>Dækopbevaring<i class="fa fa-angle-down"></i></button>
     </div> -->
   </div>
@@ -65,23 +93,37 @@ export default {
     data(){
         return{
             carData: this.data[0],
-            isOpen: false,
+            isOpen_1: false,
+            isOpen_2: false,
+            isOpen_3: false,
             monthlyPayment: this.data[0].monthlyPayment.toLocaleString('dk-DK'),
             firstPayment: this.data[0].firstPayment.toLocaleString('dk-DK'),
             ejerafgift: this.data[0].semiAnnualVehicleExciseDuty/6,
             forsikring: null,
             periode: null,
+            kmYear: null,
+            fuelPrice: null,
         }
     },
     computed: {
         total(){
             let total = ((this.carData.firstPayment/this.periode) + (this.carData.monthlyPayment*this.periode) + (this.ejerafgift*this.periode) + (this.forsikring*this.periode))
             return total.toFixed(2).replace(".", ",");
+        },
+        total_fuel(){
+            let total_fuel = ((this.kmYear/this.carData.fuelEfficiency)*this.fuelPrice)
+            return total_fuel.toFixed(2).replace(".", ",");
         }
     },
     methods: {
-        toggle(){
-            this.isOpen = !this.isOpen;
+        toggle1(){
+            this.isOpen_1 = !this.isOpen_1;
+        },
+        toggle2(){
+            this.isOpen_2 = !this.isOpen_2;
+        },
+        toggle3(){
+            this.isOpen_3 = !this.isOpen_3;
         }
     }
 }
