419ad1223c10de7587d50960565681be08f1d8c3
Freja Hyldgaard <freja.hyldgaard@autohuset-vestergaard.dk>
2022-11-30 13:12:58 +0100

    console.log removed

--- plugins/data.js
+++ plugins/data.js
@@ -5,7 +5,6 @@ export default async function ({store}){
     await axios.get("https://apim-av-ecommerce-prod.azure-api.net/vehicles/new/leasing?subscription-key=596d157bb64a4cfdbf3023bf10a91643")
     .then(response => {
         obj = response.data;
-        console.log(obj);
     })
     .catch(error => {
       console.log(error);
